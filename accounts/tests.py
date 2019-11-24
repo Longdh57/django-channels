5d447ebb28c42bf4bf4c782b19c193de2b22810e
Long Dao <long.daohai4894@gmail.com>
2019-11-24 17:38:08 +0700

    add testcase

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(username='longdao', password='secret12347', email='long.daohai4894@gmail.com')
+        User.objects.create(username='giangnguyen', password='secret12347', email='giangnguyen347@gmail.com')
+
+    def test_user(self):
+        long = User.objects.get(username='longdao')
+        kq = self.assertEqual(long.email, 'long.daohai4894@gmail.com')
